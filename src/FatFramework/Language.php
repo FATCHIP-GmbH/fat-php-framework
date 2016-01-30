35a9dd1e574e69b5c7da2fd1a6754cae40865da9
HB <HB@hendriks-imac>
2016-01-30 02:22:12 +0100

    Optimize Languag logic ignore Netbeans files

--- src/FatFramework/Language.php
+++ src/FatFramework/Language.php
@@ -5,10 +5,14 @@ namespace FatFramework;
 class Language
 {
 
-    static function getTranslation($identifier)
+    static function getTranslation($identifier, $sLang = false)
     {
+        if (!$sLang){
+            $sLang = "En";
+        }
         $sTranslatedString = $identifier . ' not found';
-        $oLanguage = new FatFramework\Language();
+        $sLangLanguage = "Language" . $sLang;
+        $oLanguage = new $sLangLanguage();
         
         if (isset($oLanguage->$identifier)) {
             $sTranslatedString = $oLanguage->$identifier;
