c1cd39ba5fb4acde59542388cde8240a80d5bfb5
Bugfighter <bahr@fatchip.de>
2016-07-20 01:05:59 +0200

    added Functions::reduceHtml() and formatCreditCardNum()
    also brought functions in alphabetical order and added some comments

--- src/FatFramework/Functions.php
+++ src/FatFramework/Functions.php
@@ -5,11 +5,51 @@ namespace FatFramework;
 class Functions
 {
     /**
-     * Get GET or POST parameters.
+     * String Trasformer
+     * Formats a 16-digit creditcard number into 4-digit-blocks
      * 
-     * @param string $sParam
+     * @param string $cc 16-digit creditcardnum
      * 
-     * @return string $sParamValue OR false
+     * @return string 4-digit-block formatted creditcardnum
+     */
+    public static function formatCreditCardNum($cc) {
+        $sDivider = ' ';
+        $cc_length = strlen($cc);
+        $newCreditCard = substr($cc, -4);
+
+        for ($i = $cc_length - 5; $i >= 0; $i--) {
+            if ((($i + 1) - $cc_length) % 4 == 0){
+                $newCreditCard = $sDivider . $newCreditCard;
+            }
+            $newCreditCard = $cc[$i] . $newCreditCard;
+        }
+
+        return $newCreditCard;
+    }
+    
+    /**
+     * String Trasformer
+     * Date string converter for dates loaded from database 
+     * and shown in templates, mails etc.
+     * 
+     * @param string $sDate Date formated like 2000-12-24
+     * 
+     * @return string $sGermanDate Date formated like 24.12.2000
+     */
+    public static function formatMysqlDate($sDate)
+    {
+        $aD = explode("-", $sDate);
+        $sGermanDate = sprintf("%02d.%02d.%04d", $aD[2], $aD[1], $aD[0]);
+        return $sGermanDate;
+    }
+    
+    /**
+     * Get cleaned GET or POST parameters.
+     * Avoid SQL-Injection etc.
+     * 
+     * @param string $sParam Parameter-name
+     * 
+     * @return string $sParamValue Parameter-value OR false
      */
     public static function getRequestParameter($sParam)
     {
@@ -21,10 +61,10 @@ class Functions
 
         if (is_string($sParamValue)) {
             $sParamValue = str_replace(
-                    array('&', '<', '>', '"', "'", chr(0), '\\', "\n", "\r"), 
-                    array('&amp;', '&lt;', '&gt;', '&quot;', '&#039;', '', '&#092;', '&#10;', '&#13;'), 
-                    $sParamValue
-                    );
+                array('&', '<', '>', '"', "'", chr(0), '\\', "\n", "\r"), 
+                array('&amp;', '&lt;', '&gt;', '&quot;', '&#039;', '', '&#092;', '&#10;', '&#13;'), 
+                $sParamValue
+                );
         }
 
         return $sParamValue;
@@ -35,6 +75,8 @@ class Functions
      * auto include all files in given directory
      * 
      * @param string $directoryPath
+     * 
+     * @return void
      */
     public static function includeDir($directoryPath)
     {
@@ -49,16 +91,21 @@ class Functions
     }
     
     /**
-     * Date string converter for dates loaded from database and shown in templates, mails etc.
+     * String Trasformer
+     * Removes whitespace and empty lines from a html-source.
      * 
-     * @param string $sDate Date formated like 2000-12-24
+     * @param string $sHtml HTML sourcecode
      * 
-     * @return string $sGermanDate Date formated like 24.12.2000
+     * @return string $sCleanedHtml Reduced HTML sourcecode
      */
-    public static function convertMysqlDate($sDate)
-    {
-        $aD = explode("-", $sDate);
-        $sGermanDate = sprintf("%02d.%02d.%04d", $aD[2], $aD[1], $aD[0]);
-        return $sGermanDate;
+    public static function reduceHtml($sHtml) {
+        $sCleanedHtml = "";
+        $aHtmlLines = explode("\n", $sHtml);
+        foreach ($aHtmlLines as $sLine) {
+            if (trim($sLine) != "") {
+                $sCleanedHtml .= trim($sLine) . "\n";
+            }
+        }
+        return $sCleanedHtml;
     }
 }
